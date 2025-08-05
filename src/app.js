025ebcee3c2b2b0d90e8414feae5a778dc848989
jardimprive <jardimprivee@gmail.com>
2025-08-05 15:24:20 -0300

    Atualiza rota admin saques e ajustes backend

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const orderRoutes = require("./routes/order.routes");
 const mpRoutes = require("./routes/mercadopago.routes");
 const authRoutes = require("./routes/auth.routes");
 const productRoutes = require("./routes/product.routes");
+const profileRoutes = require("./routes/profile.routes");
 
 const app = express();
 
@@ -38,6 +39,7 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/mercadopago", mpRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/profile", profileRoutes);
 
 app.get("/", (req, res) => {
   res.send("🌸 API Jardim Privé funcionando perfeitamente!");
