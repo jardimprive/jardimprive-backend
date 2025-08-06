d4778bd345a4b6a7e8455db02c493722c016148c
jardimprive <jardimprivee@gmail.com>
2025-08-05 21:15:04 -0300

    Corrigindo comando prisme no Vercel

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require('./src/app');
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+  console.log(`🚀 Servidor rodando na porta ${PORT}`); // Teste 
 });
